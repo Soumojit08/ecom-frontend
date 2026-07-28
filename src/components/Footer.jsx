5d1edf31217cc08503c9793375852f74960509c2
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-07-28 13:42:12 +0530

    change color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-muted  border-t">
+    <footer className="bg-secondary text-secondary-foreground  border-t">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* About Section */}
