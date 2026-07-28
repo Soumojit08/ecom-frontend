fc6d71a14c200528ec91718286e10312a18d5d0d
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-07-28 12:52:19 +0530

    changes banner text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,15 @@ import { Button } from "@/components/ui/button";
 import playstation from "../assets/PlayStation 5 Digital-Photoroom.png";
 import BlurText from "@/components/ui/BlurText";
 import { Link } from "react-router-dom";
+import { Check } from "lucide-react";
 
 const Hero = () => {
-  const handleAnimationComplete = () => {
-    console.log("Animation completed!");
-  };
+  const listItems = [
+    "Compatibility Checker",
+    "AI Recommendations",
+    "Smart PC Builder",
+    "Save & Share Builds",
+  ];
 
   return (
     <section className="h-[80vh] p-12">
@@ -14,46 +18,55 @@ const Hero = () => {
         <article id="left" className="space-y-1.5 w-2/3 ">
           <h1>
             <BlurText
-              text="Every Tech One Destination"
+              text="Build Your Perfect PC"
               delay={10}
               animateBy="words"
               direction="top"
-              onAnimationComplete={handleAnimationComplete}
-              className="capitalize text-6xl text-secondary tracking-wide font-sora"
+              className="capitalize text-6xl text-secondary tracking-wide font-sora font-semibold"
             />
           </h1>
           <h2 className="capitalize text-5xl text-secondary tracking-wide font-sora mb-4">
             <BlurText
-              text="AI-Powered Tech Marketplace"
+              text="With Confidence."
               delay={10}
               animateBy="words"
               direction="bottom"
-              onAnimationComplete={handleAnimationComplete}
-              className="capitalize text-5xl text-secondary tracking-wide font-sora"
+              className="capitalize text-6xl text-secondary tracking-wide font-sora font-semibold"
             />
           </h2>
           <p className="text-muted/80 w-4/5 text-lg mb-8">
-            Shop premium PC components, laptops, gaming gear, software,
-            peripherals, and accessories from leading brands. Then bring it all
-            together with
+            Compare components, check compatibility, receive AI-powered
+            recommendations, and build your dream PC—all from one intelligent
+            platform.
           </p>
 
           {/* cta  */}
           <div className="flex items-center gap-2.5">
             <Button
-              variant="outline"
+              variant="secondary"
               size="lg"
               className="px-12 py-6 text-xl shadow-2xl font-sora "
             >
-              <Link to="/shop">Shop Now</Link>
+              <Link to="/pc-builder">Build Your PC</Link>
             </Button>
             <Button
               variant="outline"
               size="lg"
               className="px-12 py-6 text-xl shadow-2xl text-secondary bg-transparent font-sora"
             >
-              <Link to="/pc-builder">Build Your PC</Link>
+              <Link to="/shop">Explore Products</Link>
             </Button>
+          </div>
+
+          <div className="mt-4">
+            <ul className="flex items-center space-x-6 capitalize text-secondary">
+              {listItems.map((list) => (
+                <li className="flex items-center gap-1">
+                  <Check size={16} />
+                  <span>{list}</span>
+                </li>
+              ))}
+            </ul>
           </div>
         </article>
         <div
